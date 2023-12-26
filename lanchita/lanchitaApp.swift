0fc7bb48ac093ffd0bd7886be5dee3e3c9f8a109
sachhabria <sach.chhabria@gmail.com>
2023-12-26 07:37:15 -0800

    Initial Commit

---
+++ lanchita/lanchitaApp.swift
@@ -0,0 +1,17 @@
+//
+//  lanchitaApp.swift
+//  lanchita
+//
+//  Created by Sachin Chhabria on 12/26/23.
+//
+
+import SwiftUI
+
+@main
+struct lanchitaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
